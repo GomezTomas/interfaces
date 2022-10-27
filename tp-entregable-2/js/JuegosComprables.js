784479895966a6001b9fd976333e2b8a20fc2bba
Maximiliano Otharan <maxileonel.oth@gmail.com>
2022-10-27 19:32:08 -0300

    recien comenzando el sistema de compra

---
+++ tp-entregable-2/js/JuegosComprables.js
@@ -0,0 +1,16 @@
+let juegosComprables = [
+    {
+        "nombre":"League Of Legends",
+        "precio": "500,00$"
+    },
+    {
+        "nombre":"Friday Night Funkin",
+        "precio":"1.000$"
+    },
+    {
+        "nombre":"Geometry Dash",
+        "precio":"100$"
+    },
+    {
+        
+    }]
