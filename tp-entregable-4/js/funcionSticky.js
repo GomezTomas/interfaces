7a5a2118c56ea45cafd50f192fd9137965feacc0
Maximiliano Otharan <maxileonel.oth@gmail.com>
2022-12-05 19:19:58 -0300

    parallax terminado

--- tp-entregable-4/js/funcionSticky.js
+++ tp-entregable-4/js/funcionSticky.js
@@ -40,7 +40,7 @@ function teamAnimation(y) {
             sliderAnimation.classList.toggle("carruselMove");
             sliderAnimation.classList.toggle("positionSlider");
         }
-    }else if(y > 1500 && animationActive === false) {
+    }else if(y > 2000 && animationActive === false) {
         animationActive = true;
         sliderAnimation.classList.toggle("positionSlider");
         sliderAnimation.classList.toggle("carruselMove");
@@ -89,69 +89,69 @@ let activeCard4Move2 = false;
 
 function cardMoveFeatures(y) {
     //card 1
-    if(y > 2300 && activeCardMove === false) {
+    if(y > 3300 && activeCardMove === false) {
         document.querySelector(".card").classList.toggle("cardMove");
         activeCardMove = true;
-    }else if(y < 2300 && activeCardMove === true){
+    }else if(y < 3300 && activeCardMove === true){
         document.querySelector(".card").classList.toggle("cardMove");
         activeCardMove = false;
     }
 
-    if(y > 2600 && activeCardMove2 === false) {
+    if(y > 3450 && activeCardMove2 === false) {
         document.querySelector(".card").classList.toggle("cardMove2");
         activeCardMove2 = true;
-    }else if(y < 2600 && activeCardMove2 === true){
+    }else if(y < 3450 && activeCardMove2 === true){
         document.querySelector(".card").classList.toggle("cardMove2");
         activeCardMove2 = false;
     }
 
     //card 2
-    if(y > 2800 && activeCard2Move === false) {
+    if(y > 3800 && activeCard2Move === false) {
         document.querySelector(".card-2").classList.toggle("cardMove");
         activeCard2Move = true;
-    }else if(y < 2800 && activeCard2Move === true){
+    }else if(y < 3800 && activeCard2Move === true){
         document.querySelector(".card-2").classList.toggle("cardMove");
         activeCard2Move = false;
     }
 
-    if(y > 3200 && activeCard2Move2 === false) {
+    if(y > 4000 && activeCard2Move2 === false) {
         document.querySelector(".card-2").classList.toggle("cardMove2");
         activeCard2Move2 = true;
-    }else if(y < 3200 && activeCard2Move2 === true){
+    }else if(y < 4000 && activeCard2Move2 === true){
         document.querySelector(".card-2").classList.toggle("cardMove2");
         activeCard2Move2 = false;
     }
 
     //card 3
-    if(y > 3300 && activeCard3Move === false) {
+    if(y > 4200 && activeCard3Move === false) {
         document.querySelector(".card-3").classList.toggle("cardMove");
         activeCard3Move = true;
-    }else if(y < 3300 && activeCard3Move === true){
+    }else if(y < 4200 && activeCard3Move === true){
         document.querySelector(".card-3").classList.toggle("cardMove");
         activeCard3Move = false;
     }
 
-    if(y > 3700 && activeCard3Move2 === false) {
+    if(y > 4400 && activeCard3Move2 === false) {
         document.querySelector(".card-3").classList.toggle("cardMove2");
         activeCard3Move2 = true;
-    }else if(y < 3700 && activeCard3Move2 === true){
+    }else if(y < 4400 && activeCard3Move2 === true){
         document.querySelector(".card-3").classList.toggle("cardMove2");
         activeCard3Move2 = false;
     }
 
     //card 4
-    if(y > 3800 && activeCard4Move === false) {
+    if(y > 4800 && activeCard4Move === false) {
         document.querySelector(".card-4").classList.toggle("cardMove");
         activeCard4Move = true;
-    }else if(y < 3800 && activeCard4Move === true){
+    }else if(y < 4800 && activeCard4Move === true){
         document.querySelector(".card-4").classList.toggle("cardMove");
         activeCard4Move = false;
     }
 
-    if(y > 4200 && activeCard4Move2 === false) {
+    if(y > 5000 && activeCard4Move2 === false) {
         document.querySelector(".card-4").classList.toggle("cardMove2");
         activeCard4Move2 = true;
-    }else if(y < 4200 && activeCard4Move2 === true){
+    }else if(y < 5000 && activeCard4Move2 === true){
         document.querySelector(".card-4").classList.toggle("cardMove2");
         activeCard4Move2 = false;
     }
@@ -162,11 +162,11 @@ function cardMoveFeatures(y) {
 let activateHistory = false;
 
 function historyHero(y) {
-    if(y > 500 && activateHistory === false) {
+    if(y > 1100 && activateHistory === false) {
         img.classList.toggle("inGameImgMove");
         parrafoHero.classList.toggle("parrafoHeroOpacity");
         activateHistory = true;
-    }else if(y < 500 && activateHistory === true) {
+    }else if(y < 1100 && activateHistory === true) {
         img.classList.toggle("inGameImgMove");
         parrafoHero.classList.toggle("parrafoHeroOpacity");
         activateHistory = false;
@@ -178,11 +178,11 @@ function historyHero(y) {
 let activateDescripcion = false;
 
 function descripcionHero(y) {
-    if(y > 2300 && activateDescripcion === false) {
+    if(y > 2800 && activateDescripcion === false) {
         imgAyrton.classList.toggle("gameAyrtonMove");
         parrafoAyrton.classList.toggle("parrafoAyrtonOpacity");
         activateDescripcion = true;
-    }else if(y < 2300 && activateDescripcion === true) {
+    }else if(y < 2800 && activateDescripcion === true) {
         imgAyrton.classList.toggle("gameAyrtonMove");
         parrafoAyrton.classList.toggle("parrafoAyrtonOpacity");
         activateDescripcion = false;
